665fd98e6425653edeb6d5ccbe0b22feffbf0e99
Lukesh Arora <lukesharora@Lukeshs-MacBook-Air.local>
2026-01-16 12:16:52 -0600

    removing cors origins from individual controller files

--- src/main/java/arora/software/task_master_backend/controller/AuthController.java
+++ src/main/java/arora/software/task_master_backend/controller/AuthController.java
@@ -20,7 +20,6 @@ import java.util.Map;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     @Autowired
     private UserService userService;
--- src/main/java/arora/software/task_master_backend/controller/TaskController.java
+++ src/main/java/arora/software/task_master_backend/controller/TaskController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = "http://localhost:4200")
 public class TaskController {
     private final TaskService taskService;
 
